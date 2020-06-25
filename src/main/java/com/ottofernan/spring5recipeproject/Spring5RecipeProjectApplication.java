7965d993e0534ac6f515440e6e7b59c19e665352
euseikodak <ottofernandes2012@gmail.com>
2020-06-25 14:50:51 -0300

    Beginning of the project

---
+++ src/main/java/com/ottofernan/spring5recipeproject/Spring5RecipeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ottofernan.spring5recipeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5RecipeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ottofernan/spring5recipeproject/Spring5RecipeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ottofernan.spring5recipeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
